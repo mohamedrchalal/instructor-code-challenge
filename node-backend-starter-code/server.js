270dc8cccf413c3014a5eb84c4fcb190279948ff
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-05-17 22:06:04 -0400

    search works and so does results page, must make individual movie page

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -4,8 +4,11 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+var request = require('request');
 
-app.use(express.static(path.join(__dirname, '/public')));
+
+//static route configuration so that anything in the public folder can be referenced by "/"
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -17,21 +20,36 @@ app.engine(".hbs", hbs({
   defaultLayout:  "index"
 }));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.get("/", function(req, res){
+  res.render("welcome");
+});
+
+
+app.post('/search/', function(req, res){
+  var omdbURL = "http://www.omdbapi.com/?s="+req.body.term+"&r=json"; //funnels search term to omdbapi query
+
+  request.get(omdbURL, function(err, response, body){
+    console.log(omdbURL);
+    searchRes = JSON.parse(body);
+    res.render("results", {searchRes: searchRes});
+    console.log("res", res, "bodyparsed", searchRes, "req ", req.body);
+  });
+})
 
 app.get('/favorites', function(req, res){
+  //reads files synchornously with fs
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
 app.post('/favorites', function(req, res){
-  if(!req.body.title || !req.body.imdbid){
+  if(!req.body.title || !req.body.imdbID){
     res.send("Error");
     return
   }
   var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push({title: req.body.title, id: req.body.imdbid);
+  data.push({title: req.body.title, id: req.body.imdbID});
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
